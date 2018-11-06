ffc6552248322c818d1b9b86b618e4a6f67cb3d5
jackhartmann9 <jhartmann@elon.edu>
2018-11-06 11:28:32 -0500

    Update AlphaBuild/Assets/SnakeAssets/Snake.cs

--- AlphaBuild/Assets/SnakeAssets/Snake.cs
+++ AlphaBuild/Assets/SnakeAssets/Snake.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+
+using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,10 +15,9 @@ public class Snake : MonoBehaviour {
     private float timeLeft = 60.0f;
 	public GameObject tailPrefab;
     Vector2 dir = Vector2.right;
-    // Keep Track of Tail
 	List<Transform> tail = new List<Transform>();
 
-    // Use this for initialization
+
     void Start () {
         InvokeRepeating("Move", speed, speed);    
     }
@@ -25,13 +26,13 @@ public class Snake : MonoBehaviour {
     void Update() {
         timeLeft -= Time.deltaTime;
         timeText.text = (timeLeft).ToString("0");
-    // Move in a new Direction?
+    // Move in a new Direction
 	    if (Input.GetKey(KeyCode.RightArrow))
 	        dir = Vector2.right;
 	    else if (Input.GetKey(KeyCode.DownArrow))
-	        dir = -Vector2.up;    // '-up' means 'down'
+	        dir = -Vector2.up;    
 	    else if (Input.GetKey(KeyCode.LeftArrow))
-	        dir = -Vector2.right; // '-right' means 'left'
+	        dir = -Vector2.right; 
 	    else if (Input.GetKey(KeyCode.UpArrow))
 	        dir = Vector2.up;
 	}
@@ -41,37 +42,29 @@ public class Snake : MonoBehaviour {
 
     transform.Translate(dir);
 
-    // Ate something? Then insert new Element into gap
     if (ate) {
         score++;
         scoreText.text = score.ToString();
-        // Load Prefab into the world
         GameObject g =(GameObject)Instantiate(tailPrefab,
                                               v,
                                               Quaternion.identity);
 
-        // Keep track of it in our tail list
         tail.Insert(0, g.transform);
 
         GameObject g1 =(GameObject)Instantiate(tailPrefab,
                                               v,
                                               Quaternion.identity);
 
-        // Keep track of it in our tail list
+   
         tail.Insert(0, g1.transform);
-
         GameObject g2 =(GameObject)Instantiate(tailPrefab,
                                               v,
                                               Quaternion.identity);
-
-        // Keep track of it in our tail list
+ 
         tail.Insert(0, g2.transform);
-
-        // Reset the flag
         ate = false;
         speed = speed * .7f;
     }
-    // Do we have a Tail?
     else if (tail.Count > 0) {
         // Move last Tail Element to where the Head was
         tail.Last().position = v;
@@ -85,16 +78,14 @@ public class Snake : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D coll) {
         // Food?
         if (coll.name.StartsWith("FoodPrefab")) {
-            // Get longer in next Move call
             ate = true;
-            
-            // Remove the Food
             Destroy(coll.gameObject);
         }
-        // Collided with Tail or Border
+        
         if (coll.gameObject.tag == "EndGame") {
         	Debug.Log("Dead");
             Destroy(this);
+            SceneManager.LoadScene("MainMenu");
             // ToDo 'You lose' screen
         }
 	}
